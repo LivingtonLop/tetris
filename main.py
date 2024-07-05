7edc94a3844a60161830b367444a4a442643fb95
Livington <lopezcruzlivingtonlenin@gmail.com>
2024-07-05 11:23:08 -0500

    Puntaje

--- main.py
+++ main.py
@@ -3,17 +3,24 @@
 import pygame
 import random
 
+pause = False
 #Configure this must be in file.env o Enviorement Variable
-WIDTH : int = 300 #px
+WIDTH : int = 600 #px
 HEIGHT : int = 600 #px
+WIDTH_TABLET = 300
 SIZE_CELL : int = 30 #px
-COLUMN_TABLET :int = WIDTH//SIZE_CELL
+COLUMN_TABLET :int = WIDTH_TABLET//SIZE_CELL
 ROW_TABLET :int = HEIGHT//SIZE_CELL
 
 #colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
+boton_x = 370
+boton_y = 50
+
+img_retry = pygame.image.load("retry.png")
+img_retry = pygame.transform.scale(img_retry, (100, 100))
 
 #Asset (colores) -> new folder in project like : assets/texture, this project is in 2d, alone colors simples
 
@@ -96,78 +103,105 @@ def rotate_piece(piece):
 
 #verificate pieces row
 def verificate_delete_row_complete(tablet):
-    count_row = 0
+    row_complete = 0
     for y in range(ROW_TABLET):
         if all(tablet[y][x] != BLACK for x in range(COLUMN_TABLET)):
-            count_row += 1
+            row_complete += 1
             # Eliminar la fila completa y desplazar las superiores hacia abajo
             for mover_y in range(y, 0, -1):
                 tablet[mover_y] = tablet[mover_y - 1]
             tablet[0] = [BLACK for _ in range(COLUMN_TABLET)]
-    return count_row
+            
+    return row_complete
+
+#show menu
+def show_score(screen, score, coor):
+    font = pygame.font.SysFont('Arial', 24)
+    text = font.render(f'Puntuación: {score}', True, BLACK)
+    screen.blit(text, coor)
+
+# def reiniciar_juego():
+#     [[BLACK for _ in range(COLUMN_TABLET)] for _ in range(ROW_TABLET)]
+#     new_piece()
+    
+
 
 
 # Función principal del juego
 def main():
     tablet = [[BLACK for _ in range(COLUMN_TABLET)] for _ in range(ROW_TABLET)]
     piece_now = new_piece()
-    
+    score = 0
     #clock
     clock = pygame.time.Clock()
     time_down = 1000 
     time_prev = pygame.time.get_ticks()
-    
+
     ejecutando = True
     while ejecutando:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 ejecutando = False
-            #keyboards
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_LEFT:
-                    piece_now.x -= 1
-                    if collision(tablet=tablet,piece= piece_now):
-                        piece_now.x += 2
-                if evento.key == pygame.K_RIGHT:
-                    piece_now.x += 1
-                    if collision(tablet=tablet,piece=  piece_now):
-                        piece_now.x -= 2
-                if evento.key == pygame.K_DOWN:
-                    piece_now.y += 1
-                    if collision(tablet=tablet,piece=  piece_now):
-                        piece_now.y -= 1
-                if evento.key == pygame.K_UP:
-                    rotate_piece(piece_now)
-                    if collision(tablet=tablet,piece=  piece_now):
-                        for _ in range(3):
-                            rotate_piece(piece_now)
 
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if evento.button == 1:
+                    mouse_x, mouse_y = evento.pos
+                    if boton_x <= mouse_x <= boton_x + img_retry.get_width() and boton_y <= mouse_y <= boton_y + img_retry.get_height():
+                        print("si")
+                        
+                    
+            if not pause:
+                #keyboards
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_LEFT:
+                        piece_now.x -= 1
+                        if collision(tablet=tablet,piece= piece_now):
+                            piece_now.x += 2
+                    if evento.key == pygame.K_RIGHT:
+                        piece_now.x += 1
+                        if collision(tablet=tablet,piece=  piece_now):
+                            piece_now.x -= 2
+                    if evento.key == pygame.K_DOWN:
+                        piece_now.y += 1
+                        if collision(tablet=tablet,piece=  piece_now):
+                            piece_now.y -= 1
+                    if evento.key == pygame.K_UP:
+                        rotate_piece(piece_now)
+                        if collision(tablet=tablet,piece=  piece_now):
+                            for _ in range(3):
+                                rotate_piece(piece_now)
 
-        time_now = pygame.time.get_ticks() 
-        if time_now - time_prev >= time_down:
-            piece_now.y +=1
-            if collision(tablet=tablet, piece=piece_now):
-                piece_now.y -= 1
-                add_piece(tablet=tablet,piece=piece_now)
-                piece_now = new_piece()
-                verificate_delete_row_complete(tablet=tablet)
+        if not pause:
+            time_now = pygame.time.get_ticks() 
+            if time_now - time_prev >= time_down:
+                piece_now.y +=1
                 if collision(tablet=tablet, piece=piece_now):
-                    print("Game Over")
-                    ejecutando = False
+                    piece_now.y -= 1
+                    add_piece(tablet=tablet,piece=piece_now)
 
-            time_prev = time_now
+                    row_del = verificate_delete_row_complete(tablet=tablet)
+                    score += row_del*100
 
+                    piece_now = new_piece()
 
+                    if collision(tablet=tablet, piece=piece_now):
+                        ejecutando = False
+
+                time_prev = time_now
 
         screen.fill(WHITE)
+            
         draw_tablet(screen, tablet)
         draw_piece(screen, piece_now)
+        show_score(screen=screen,score=score, coor=(350,10))
+        screen.blit(img_retry,(boton_x,boton_y))
+
         pygame.display.update()
         
         clock.tick(60)
 
-    #show retry
-    print("Vuelve a intentar")
+    
+    
 
 main()
 pygame.quit()
